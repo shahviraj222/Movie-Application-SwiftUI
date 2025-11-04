5ee340eb999c8fbfdfe8b3da84907eb1070546e2
shahviraj222 <viraj22feb@gmail.com>
2025-11-04 15:14:29 +0530

    Initial Commit

---
+++ Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie
+//
+//  Created by viraj shah on 04/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
