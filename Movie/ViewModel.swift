3858eae2ee83cd34f2364274afcf997cb14afb6a
shahviraj222 <viraj22feb@gmail.com>
2025-11-08 11:38:21 +0530

    view model created to keep track

--- Movie/ViewModel.swift
+++ Movie/ViewModel.swift
@@ -5,4 +5,33 @@
 //  Created by viraj shah on 08/11/25.
 //
 
+//here Obeservalbe keep update all propety in view.
+//here private(set) make only read propety
+
 import Foundation
+
+
+@Observable
+class ViewModel{
+    enum FetchStatus{
+        case notStarted
+        case fetching
+        case success
+        case failed(underlyingError:Error)
+    }
+    private(set) var homeStatus:FetchStatus = .notStarted
+    private let dataFetcher = DataFetcher()
+    var trendingMoveis:[Title] = []
+    
+    func getTitles() async {
+        homeStatus = .fetching
+        
+        do{
+            trendingMoveis = try await dataFetcher.fetchTitles(for: "movie")
+            homeStatus = .success
+        }catch{
+            print(error)
+            homeStatus = .failed(underlyingError: error)
+        }
+    }
+}
