d613d9a6ff4e16abe5f11be89c649171de1f0e10
shahviraj222 <viraj22feb@gmail.com>
2025-11-08 11:37:35 +0530

    data fetcher completed

--- Movie/DataFetcher.swift
+++ Movie/DataFetcher.swift
@@ -7,13 +7,49 @@
 
 //  Async function works on mutiple thread here
 
+//  URLSession.shared The shared property comes from URLSession itself - it's a built-in singleton instance provided by Apple's Foundation framework.
 
+//  NSError is nextstep error in that we have 3 paramtere domain , code, userInfo
+
+//  Decoding stategy    TMDB API (like many REST APIs) uses snake_case naming convention Swift conventionally uses camelCase for property names This strategy bridges the gap automatically
+                                            
 import Foundation
 
-let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
-let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
-
-
-func fetchTitles(for media:String) async throws -> [Title]{
+struct DataFetcher{
+    let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
+    let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
+    
+    
+    func fetchTitles(for media:String) async throws -> [Title]{
+        guard let baseURL = tmdbBaseURL else{
+            throw NetworkError.missingConfig
+        }
+        guard let apiKey = tmdbAPIKey else{
+            throw NetworkError.missingConfig
+        }
+        guard let fetchTitlesURL = URL(string:baseURL)?
+            .appending(path:"3/trending/\(media)/day")
+            .appending(queryItems: [
+                URLQueryItem(name:"api_key",value: apiKey)
+            ])else{
+            throw NetworkError.urlBuildFailed
+        }
+        print(fetchTitlesURL)
+        
+        let(data,urlResponse) = try await URLSession.shared.data(from:fetchTitlesURL)
+        
+        guard let response = urlResponse as? HTTPURLResponse, response.statusCode == 200 else{
+            throw NetworkError.badURLResponse(underlyingError: NSError(
+                domain: "DataFetcher",
+                code: (urlResponse as? HTTPURLResponse)?.statusCode ?? -1,
+                userInfo: [NSLocalizedDescriptionKey:"Invalid Http Resoponse"]))
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        var titles =  try decoder.decode(APIObject.self, from: data).results
+        Constants.addPosterPath(to: &titles)                // passing the address
+        return titles
+    }
     
 }
---
+++ Movie/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Movie
+//
+//  Created by viraj shah on 08/11/25.
+//
+
+import Foundation
